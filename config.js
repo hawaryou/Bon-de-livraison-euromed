ef9e4f6a1c4d533c513b7a19110fdc654ec4432b
hawaryou <hawaryougaming@gmail.com>
2026-09-25 16:35:53 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Configuration du Bon de livraison EuroMed.
 // Les notices/protocoles sont stockés dans un dépôt GitHub PUBLIC.
-window.GOOGLE_APPS_SCRIPT_URL = 'https://github.com/hawaryou/bon-livraison-documents';
+window.GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyXI3t-K-_F9iZPNkDY65BocTJlElQZw3WRsEU33WMlJq_awQ49huRXt07_IP6Yw0Te/exec';
 
 // Adresse EuroMed qui reçoit TOUS les bons de livraison.
 window.EUROMED_EMAIL = 'valentineuromed@gmail.com';
